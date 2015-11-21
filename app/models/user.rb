65200f4e089385b752f2d29ca33d751341d35e3a
Dominic Michalec <dominicjjmichalec@gmail.com>
2015-11-21 16:35:48 -0500

    clean up navbar filters

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   before_save { self.email = email.downcase }
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable
+         :rememberable, :trackable
   VALID_EMAIL_REGEX = /\A[a-zA-Z]+@covermymeds\.com\z/
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX, message: "must be your @covermymeds.com work email" },
   uniqueness: { case_sensitive: false }
