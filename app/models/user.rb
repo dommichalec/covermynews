59c26adc461a5645ee5ab37b396cbd05f04e5853
Dominic Michalec <dominicjjmichalec@gmail.com>
2015-11-21 11:39:44 -0500

    add devise and user model

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ActiveRecord::Base
+  before_save { self.email = email.downcase }
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable
+  VALID_EMAIL_REGEX = /\A[a-zA-Z]+@covermymeds\.com\z/
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX, message: "must be your @covermymeds.com work email" },
+  uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 8 }
+end
