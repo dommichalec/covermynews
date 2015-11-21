0f9fa97c1dd1cf9c53b1204d307b5d688b07f412
Dominic Michalec <dominicjjmichalec@gmail.com>
2015-11-21 16:15:46 -0500

    add category/link association

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151121185437) do
+ActiveRecord::Schema.define(version: 20151121204704) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "comments", force: :cascade do |t|
     t.integer  "link_id"
@@ -27,9 +33,10 @@ ActiveRecord::Schema.define(version: 20151121185437) do
   create_table "links", force: :cascade do |t|
     t.string   "title"
     t.string   "url"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.integer  "user_id"
+    t.integer  "category_id"
   end
 
   add_index "links", ["user_id"], name: "index_links_on_user_id"
