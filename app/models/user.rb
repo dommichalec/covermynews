ac436d9a5268e144c0f96fb279f512ce1c6fb08e
Dominic Michalec <dominicjjmichalec@gmail.com>
2015-11-21 11:57:23 -0500

    add link and user association

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ActiveRecord::Base
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX, message: "must be your @covermymeds.com work email" },
   uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 8 }
+
+  has_many :links
 end
