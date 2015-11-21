b2e5bc7a1b4c87d47a04041118f182082cb709d3
Dominic Michalec <dominicjjmichalec@gmail.com>
2015-11-21 15:18:34 -0500

    changeover from email to name

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :comments
   devise_for :users
+  resources :comments
   resources :links do
     member do
       put "like", to:    "links#upvote"
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151121181556) do
+ActiveRecord::Schema.define(version: 20151121185437) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "link_id"
@@ -47,6 +47,8 @@ ActiveRecord::Schema.define(version: 20151121181556) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "first_name"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
